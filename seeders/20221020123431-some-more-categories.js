8df436715a5e52022207a70006c02bf63f9eacbf
jmaguero <juanmaguero@hotmail.com>
2022-10-21 16:00:31 +0200

    Endpoints modified

--- seeders/20221020123431-some-more-categories.js
+++ seeders/20221020123431-some-more-categories.js
@@ -5,41 +5,28 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('categories', [
       {
-        title: "Furniture",
-        description: "Variated furniture. For home and work",
-        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX1XN-pJARemwC6ESqBR5uGtwMkVZ2NBrpg&usqp=CAU",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        title: "Home",
-        description: "All for your home",
-        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX1XN-pJARemwC6ESqBR5uGtwMkVZ2NBrpg&usqp=CAU",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        title: "Outdoors",
-        description: "Outdoors Utils",
+        title: "Bikes",
+        description: "Used bikes",
         imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX1XN-pJARemwC6ESqBR5uGtwMkVZ2NBrpg&usqp=CAU",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         title: "Tools",
-        description: "Variated tools",
+        description: "Used tools",
         imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX1XN-pJARemwC6ESqBR5uGtwMkVZ2NBrpg&usqp=CAU",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        title: "Sports",
-        description: "Variated Sports",
+        title: "Computers",
+        description: "Used computers",
         imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX1XN-pJARemwC6ESqBR5uGtwMkVZ2NBrpg&usqp=CAU",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-    ]);
+    ]
+    )
   },
 
   async down(queryInterface, Sequelize) {
